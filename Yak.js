509920ea8a67ae00c8f45bc80dd15ea0627b88c9
wrenchorizo <goldenboyf6@gmail.com>
2026-03-14 20:13:07 -0600

    Update Yak.js

--- Yak.js
+++ Yak.js
@@ -561,6 +561,9 @@ ${prefix}givechar @usuario [Nombre]
 ${prefix}trade @usuario [MiChar] | [SuChar]
 > Intercambio de personajes.
 
+${prefix}aceptartrade
+> Aceptar un trade.
+
 ${prefix}smob
 > Busca mobs para pelear contra ellos y conseguir recompensas.
 
@@ -2798,7 +2801,7 @@ if (listaReacciones.includes(comandoLimpio)) {
 // --- DETECTOR DE COMANDO INEXISTENTE ---
     if (message.body.startsWith(prefix)) {
         const comandoBase = comando.split(/\s+/)[0];
-        const misComandos = ['duel', 'rw', 'harem', 'wimage', 'shop', 'gay', 'kick', 'bal', 'baltop', 'buy', 'crime', 'daily', 'c', 'help', 'menu', 'cal', 'ping', 'charinfo', 'charlist', 'pay', 'cooldowns', 'w', 'pokevo', 'accept', 'pick', 'yt', 's', 'say', 'tr', 'dice', 'smob', 'fight', 'reload', 'addmoney', 'charshop', 'bchar', 'givechar'];
+        const misComandos = ['duel', 'rw', 'harem', 'wimage', 'aceptartrade', 'shop', 'gay', 'kick', 'bal', 'baltop', 'buy', 'crime', 'daily', 'c', 'help', 'menu', 'cal', 'ping', 'charinfo', 'charlist', 'pay', 'cooldowns', 'w', 'pokevo', 'accept', 'pick', 'yt', 's', 'say', 'tr', 'dice', 'smob', 'fight', 'reload', 'addmoney', 'charshop', 'bchar', 'givechar'];
         
         if (!misComandos.includes(comandoBase) && !listaReacciones.includes(comandoBase)) {
             return message.reply(`⌦ El comando *${prefix}${comandoBase}* no existe.\n Usa *${prefix}help* para ver la lista de comandos`);
